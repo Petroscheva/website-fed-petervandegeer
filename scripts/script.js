6bd63ee27f3b4cd381e611366dc997b42ae472fc
Petroscheva <pjhvandegeer@gmail.com>
2025-11-25 15:20:43 +0100

    bezig met knoppen

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,20 @@
-// JavaScript Document
-console.log("hi");
+const dialog = document.querySelector("dialog");
+const nieuw = document.getElementById("sectie1")
+const themas = document.getElementById("sectie2")
+
+//van nieuw naar themas//
+function KlikopThemas() {
+  themas.classList.remove("hidden")
+  nieuw.classList.add("hidden")
+}
+//van themas naar nieuw//
+function KlikopNieuw() {
+  nieuw.classList.remove("hidden")
+  themas.classList.add("hidden")
+}
+
+
+//wanneer de pagina laad komt er een modal tevoorschijn//
+window.addEventListener("load", () => {
+  dialog.showModal();
+});
